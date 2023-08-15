01681633f25410ac4069853fd17bd6eeb1066e53
liguangliang <13537223157@163.com>
2023-08-15 17:16:20 +0800

    feat: add test module proto

--- internal/test-service/biz/review.go
+++ internal/test-service/biz/review.go
@@ -36,13 +36,14 @@ type TestReviewRepo interface {
 	DeleteTestReview(ctx context.Context, id int64) error
 }
 type TestReviewCase struct {
-	Id       int64     `json:"id" comment:"测试报告id"`
-	ReviewId int64     `json:"review_id" comment:"review_id"`
-	CaseId   int64     `json:"name" comment:"测试用例id"`
-	Status   string    `json:"status" comment:"状态"`
-	Deleted  int64     `json:"deleted"`
-	CreateAt time.Time `json:"create_at"`
-	UpdateAt time.Time `json:"update_at"`
+	Id            int64     `json:"id" comment:"测试报告id"`
+	ReviewId      int64     `json:"review_id" comment:"review_id"`
+	CaseId        int64     `json:"name" comment:"测试用例id"`
+	Status        string    `json:"status" comment:"状态"`
+	Deleted       int64     `json:"deleted"`
+	ReviewedEndAt time.Time `json:"reviewed_end_at"`
+	CreateAt      time.Time `json:"create_at"`
+	UpdateAt      time.Time `json:"update_at"`
 }
 type TestReviewCaseDTOList struct {
 	TotalCount int64             `json:"totalCount,omitempty"` //总数
